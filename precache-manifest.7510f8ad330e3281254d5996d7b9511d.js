38e41c924035a697ce903979c5e4ed89a9e205a8
Konrad Duleba <kon.duleba@gmail.com>
2020-11-28 15:40:41 +0100

    Updates

---
+++ precache-manifest.7510f8ad330e3281254d5996d7b9511d.js
@@ -0,0 +1,302 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a07a94fab4627efa005727c9b3abc51e",
+    "url": "/BudgetApp/index.html"
+  },
+  {
+    "revision": "3a1b56c58e3c3fd5c673",
+    "url": "/BudgetApp/static/css/4.8921c4c4.chunk.css"
+  },
+  {
+    "revision": "d0ba4f0c587c3cf697d1",
+    "url": "/BudgetApp/static/css/main.a4a49469.chunk.css"
+  },
+  {
+    "revision": "e1a29aa77ed18396d923",
+    "url": "/BudgetApp/static/js/0.c7f647e1.chunk.js"
+  },
+  {
+    "revision": "bca28cc3228e259ec8ba",
+    "url": "/BudgetApp/static/js/10.4477ff42.chunk.js"
+  },
+  {
+    "revision": "ca63e9d6cf388aa9720d",
+    "url": "/BudgetApp/static/js/11.434a228e.chunk.js"
+  },
+  {
+    "revision": "312358f2f29d750f3948",
+    "url": "/BudgetApp/static/js/12.b1024062.chunk.js"
+  },
+  {
+    "revision": "b8339169579b935452fa",
+    "url": "/BudgetApp/static/js/13.5d59843f.chunk.js"
+  },
+  {
+    "revision": "b0ac72fa7a57b2ebef4e",
+    "url": "/BudgetApp/static/js/14.9ce47a21.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/BudgetApp/static/js/14.9ce47a21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "329c908318d5859a82ef",
+    "url": "/BudgetApp/static/js/15.dcd11323.chunk.js"
+  },
+  {
+    "revision": "8643d77339b57d9346bb",
+    "url": "/BudgetApp/static/js/16.6bb6df79.chunk.js"
+  },
+  {
+    "revision": "65534ea3ca641612b9e1",
+    "url": "/BudgetApp/static/js/17.3b0f3ccc.chunk.js"
+  },
+  {
+    "revision": "14872bbf90c5c3923715",
+    "url": "/BudgetApp/static/js/18.62cdde57.chunk.js"
+  },
+  {
+    "revision": "533973cf26189cc04b0c",
+    "url": "/BudgetApp/static/js/19.9118b762.chunk.js"
+  },
+  {
+    "revision": "a87cbb973349bb9feb98",
+    "url": "/BudgetApp/static/js/20.451c6d27.chunk.js"
+  },
+  {
+    "revision": "50c61d2e71e86e028c94",
+    "url": "/BudgetApp/static/js/21.3f78be2e.chunk.js"
+  },
+  {
+    "revision": "b1b65fcb9f4354af1d90",
+    "url": "/BudgetApp/static/js/22.d79667f4.chunk.js"
+  },
+  {
+    "revision": "4dcaff4c7bdca12f04a5",
+    "url": "/BudgetApp/static/js/23.1f055f19.chunk.js"
+  },
+  {
+    "revision": "08a48e50728b86e5b379",
+    "url": "/BudgetApp/static/js/24.b71ce6c3.chunk.js"
+  },
+  {
+    "revision": "acec18c6b7abd233318b",
+    "url": "/BudgetApp/static/js/25.48bc82b8.chunk.js"
+  },
+  {
+    "revision": "5399f99d7cad77e3d846",
+    "url": "/BudgetApp/static/js/26.6907c793.chunk.js"
+  },
+  {
+    "revision": "efa80a8a9706d9b6159e",
+    "url": "/BudgetApp/static/js/27.47e713de.chunk.js"
+  },
+  {
+    "revision": "fe5a342313d9884b5b53",
+    "url": "/BudgetApp/static/js/28.376e937f.chunk.js"
+  },
+  {
+    "revision": "33c15ae569d646f6b1a3",
+    "url": "/BudgetApp/static/js/29.0d6da477.chunk.js"
+  },
+  {
+    "revision": "b7ee5691fcd474cbab6a",
+    "url": "/BudgetApp/static/js/30.c3a729e3.chunk.js"
+  },
+  {
+    "revision": "f03605fdd246ecef3453",
+    "url": "/BudgetApp/static/js/31.9fa0c0f8.chunk.js"
+  },
+  {
+    "revision": "f842b366bf6755c960d3",
+    "url": "/BudgetApp/static/js/32.108c334b.chunk.js"
+  },
+  {
+    "revision": "aebbce8612d98c446988",
+    "url": "/BudgetApp/static/js/33.372e19e6.chunk.js"
+  },
+  {
+    "revision": "cdbb53542c1cab917ae0",
+    "url": "/BudgetApp/static/js/34.92923e2c.chunk.js"
+  },
+  {
+    "revision": "0b50ab75d30b10a0b87a",
+    "url": "/BudgetApp/static/js/35.2c9a3187.chunk.js"
+  },
+  {
+    "revision": "b25526cbea40729d4570",
+    "url": "/BudgetApp/static/js/36.c36e79f2.chunk.js"
+  },
+  {
+    "revision": "8e31f2758c565ac1a754",
+    "url": "/BudgetApp/static/js/37.df1180d7.chunk.js"
+  },
+  {
+    "revision": "a85d44a76659cf0ba1b3",
+    "url": "/BudgetApp/static/js/38.0b1bb00a.chunk.js"
+  },
+  {
+    "revision": "69687c32299391555fc1",
+    "url": "/BudgetApp/static/js/39.6c6852cd.chunk.js"
+  },
+  {
+    "revision": "3a1b56c58e3c3fd5c673",
+    "url": "/BudgetApp/static/js/4.79d74654.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/BudgetApp/static/js/4.79d74654.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ca6cebb87d59b276000",
+    "url": "/BudgetApp/static/js/40.d41e8fbb.chunk.js"
+  },
+  {
+    "revision": "7a0738d004513d0241b6",
+    "url": "/BudgetApp/static/js/41.e8062abb.chunk.js"
+  },
+  {
+    "revision": "881f48c0c02c7ad81ec3",
+    "url": "/BudgetApp/static/js/42.4f715968.chunk.js"
+  },
+  {
+    "revision": "fa03b7aeb56b49f3689c",
+    "url": "/BudgetApp/static/js/43.62750923.chunk.js"
+  },
+  {
+    "revision": "122ce52eb77cd0e70bcb",
+    "url": "/BudgetApp/static/js/44.863617ca.chunk.js"
+  },
+  {
+    "revision": "99b43b6712794950ff6e",
+    "url": "/BudgetApp/static/js/45.a4101e28.chunk.js"
+  },
+  {
+    "revision": "c6dd64cec4d360344519",
+    "url": "/BudgetApp/static/js/46.2ed68267.chunk.js"
+  },
+  {
+    "revision": "9b3a1bfade03ff5d17e9",
+    "url": "/BudgetApp/static/js/47.e68e308d.chunk.js"
+  },
+  {
+    "revision": "7c88aee08f265fc3caee",
+    "url": "/BudgetApp/static/js/48.8417da33.chunk.js"
+  },
+  {
+    "revision": "426fa83305c503718a6a",
+    "url": "/BudgetApp/static/js/49.7891558a.chunk.js"
+  },
+  {
+    "revision": "018cf65b98f9c83d653c",
+    "url": "/BudgetApp/static/js/5.a6a0a87d.chunk.js"
+  },
+  {
+    "revision": "c3151e8f5a90c1e591d6",
+    "url": "/BudgetApp/static/js/50.a02de454.chunk.js"
+  },
+  {
+    "revision": "00ea5f5875d3b0a99b55",
+    "url": "/BudgetApp/static/js/51.474bd632.chunk.js"
+  },
+  {
+    "revision": "a64e5890c3777286eca5",
+    "url": "/BudgetApp/static/js/52.6d2305eb.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/BudgetApp/static/js/52.6d2305eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe8912d52eff46b39668",
+    "url": "/BudgetApp/static/js/53.e66a8172.chunk.js"
+  },
+  {
+    "revision": "38ac23f335628ac5189f",
+    "url": "/BudgetApp/static/js/54.4068b49f.chunk.js"
+  },
+  {
+    "revision": "4d2021ee85755588627b",
+    "url": "/BudgetApp/static/js/55.a0c4dd2f.chunk.js"
+  },
+  {
+    "revision": "4b2f608f800fd5769d5b",
+    "url": "/BudgetApp/static/js/56.92ce3b95.chunk.js"
+  },
+  {
+    "revision": "48629624390c18556ccb",
+    "url": "/BudgetApp/static/js/57.2f43b480.chunk.js"
+  },
+  {
+    "revision": "9a83ee55a3e1f71b716d",
+    "url": "/BudgetApp/static/js/58.f81a508c.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/BudgetApp/static/js/58.f81a508c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73eb19bbc19dc66f8dcf",
+    "url": "/BudgetApp/static/js/6.3be529d3.chunk.js"
+  },
+  {
+    "revision": "af93d6816b284fd3e285",
+    "url": "/BudgetApp/static/js/7.22247211.chunk.js"
+  },
+  {
+    "revision": "48e58e5e81321fe894d6",
+    "url": "/BudgetApp/static/js/8.de507cda.chunk.js"
+  },
+  {
+    "revision": "bdd116089eb50980cd67",
+    "url": "/BudgetApp/static/js/9.4c0c36ab.chunk.js"
+  },
+  {
+    "revision": "d0ba4f0c587c3cf697d1",
+    "url": "/BudgetApp/static/js/main.f36affb1.chunk.js"
+  },
+  {
+    "revision": "ea1a015270abad109404",
+    "url": "/BudgetApp/static/js/polyfills-css-shim.965e90e3.chunk.js"
+  },
+  {
+    "revision": "08db0735a80b1b855036",
+    "url": "/BudgetApp/static/js/runtime-main.19799b6c.js"
+  },
+  {
+    "revision": "e94ce7a03ff17721b39cb91c8bc1e53a",
+    "url": "/BudgetApp/static/media/android.e94ce7a0.png"
+  },
+  {
+    "revision": "1f14def94db661ce28851121efc3d311",
+    "url": "/BudgetApp/static/media/app_logo.1f14def9.png"
+  },
+  {
+    "revision": "445cef5db2118e846f8e8f7b1c713fe0",
+    "url": "/BudgetApp/static/media/app_store.445cef5d.png"
+  },
+  {
+    "revision": "c2e70a6b7154b9492eb01e8b3f02ba34",
+    "url": "/BudgetApp/static/media/calculate.c2e70a6b.png"
+  },
+  {
+    "revision": "c78de86b2e8055c4940ac174633c18b6",
+    "url": "/BudgetApp/static/media/control.c78de86b.png"
+  },
+  {
+    "revision": "89675a3a9ad7d3913c18fd76869054d6",
+    "url": "/BudgetApp/static/media/empty_wallet.89675a3a.png"
+  },
+  {
+    "revision": "754d705eff84d1566918429616852ed0",
+    "url": "/BudgetApp/static/media/google_play_logo.754d705e.png"
+  },
+  {
+    "revision": "d821ade36d1d51d8c19a9b2739a9377a",
+    "url": "/BudgetApp/static/media/iphone.d821ade3.png"
+  },
+  {
+    "revision": "ffc6879fb108dd94e96d6399225e990c",
+    "url": "/BudgetApp/static/media/planning3.ffc6879f.png"
+  }
+]);
